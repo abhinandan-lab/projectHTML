e7b2841b19fabd134b4af8d6c6a18b9aa74a57a4
abhinandan-lab <airindustry3@gmail.com>
2023-03-07 15:36:14 +0530

    update1

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-function toggleModal(evt, idname) {
+function toggleModal(evt, idname, mybtn) {
 
     let tabs = document.querySelectorAll('.tabcontent');
 
-    tabs.forEach(element => {
+    let navbtns = document.querySelector('#main-head').children;
+
+    for (let index = 0; index < tabs.length; index++) {
+        const element = tabs[index];
         if(element.id == idname) {
             element.style.display = 'block';
+            const list = navbtns[index].classList;
+            list.add("btn-active");   
         }
+
         else {
             element.style.display = 'none';
+            navbtns[index].className = '';
+        }   
+    }
+}
+
+
+function toggleSidebar(evt, idname){
+
+    let tabs = document.querySelectorAll('.tabcontent-sidebar');
+
+    let navbtns = document.querySelector('#sidebarbtns').children;
+
+    for (let index = 0; index < tabs.length; index++) {
+        const element = tabs[index];
+        if(element.id == idname) {
+            element.style.display = 'flex';
+            const list = navbtns[index].classList;
+            list.add("btn-active");   
         }
-    });
+
+        else {
+            element.style.display = 'none';
+            navbtns[index].className = '';
+        }   
+    }
 }
